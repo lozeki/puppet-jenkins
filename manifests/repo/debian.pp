742ecd1ec5e8404aa00a2b01a9d19717ae90ba2e
lozeki <minhtri.ng@gmail.com>
2023-09-01 09:46:41 -0400

    Update debian.pp

--- manifests/repo/debian.pp
+++ manifests/repo/debian.pp
@@ -6,31 +6,47 @@ class jenkins::repo::debian
     fail("Use of private class ${name} by ${caller_module_name}")
   }
 
-  include stdlib
-  include apt
+  include ::stdlib
+  include ::apt
+
+  $pkg_host = 'https://pkg.jenkins.io'
+
+  ensure_packages(['apt-transport-https'])
 
   if $::jenkins::lts  {
     apt::source { 'jenkins':
-      location    => 'https://pkg.jenkins.io/debian-stable',
-      release     => 'binary/',
-      repos       => '',
-      key         => '63667EE74BBA1F0A08A698725BA31D57EF5975CA',
-      key_source  => 'https://pkg.jenkins.io/debian/jenkins.io.key',
-      include_src => false,
+      location => "${pkg_host}/debian-stable",
+      release  => 'binary/',
+      repos    => '',
+      include  => {
+        'src' => false,
+      },
+      key      => {
+        'id'     => '150FDE3F7787E7D11EF4E12A9B7D32F2D50582E6',
+        'source' => "${pkg_host}/debian/jenkins-ci.org.key",
+      },
+      require  => Package['apt-transport-https'],
+      notify   => Exec['apt_update'],
     }
   }
   else {
     apt::source { 'jenkins':
-      location    => 'https://pkg.jenkins.io/debian',
-      release     => 'binary/',
-      repos       => '',
-      key         => '63667EE74BBA1F0A08A698725BA31D57EF5975CA',
-      key_source  => 'https://pkg.jenkins.io/debian/jenkins.io.key',
-      include_src => false,
+      location => "${pkg_host}/debian",
+      release  => 'binary/',
+      repos    => '',
+      include  => {
+        'src' => false,
+      },
+      key      => {
+        'id'     => '150FDE3F7787E7D11EF4E12A9B7D32F2D50582E6',
+        'source' => "${pkg_host}/debian/jenkins-ci.org.key",
+      },
+      require  => Package['apt-transport-https'],
+      notify   => Exec['apt_update'],
     }
   }
 
-  anchor { 'jenkins::repo::debian::begin': } ->
-    Apt::Source['jenkins'] ->
-    anchor { 'jenkins::repo::debian::end': }
+  anchor { 'jenkins::repo::debian::begin': }
+    -> Apt::Source['jenkins']
+    -> anchor { 'jenkins::repo::debian::end': }
 }
