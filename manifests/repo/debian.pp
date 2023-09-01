769e8c9113b3da35344f5a73b678aa539e36c521
lozeki <minhtri.ng@gmail.com>
2023-09-01 11:25:27 -0400

    Update debian.pp

--- manifests/repo/debian.pp
+++ manifests/repo/debian.pp
@@ -1,26 +1,44 @@
-class jenkins::repo::debian (
-  String $gpg_key_id = '63667EE74BBA1F0A08A698725BA31D57EF5975CA',
-) {
-  assert_private()
-
-  include apt
-
-  if $jenkins::lts {
-    $location = "${jenkins::repo::base_url}/debian-stable"
-  } else {
-    $location = "${jenkins::repo::base_url}/debian"
+# Class: jenkins::repo::debian
+#
+class jenkins::repo::debian
+{
+  if $caller_module_name != $module_name {
+    fail("Use of private class ${name} by ${caller_module_name}")
   }
 
-  apt::source { 'jenkins':
-    location => $location,
-    release  => 'binary/',
-    repos    => '',
-    include  => {
-      'src' => false,
-    },
-    key      => {
-      'id'     => $gpg_key_id,
-      'source' => "${location}/${jenkins::repo::gpg_key_filename}",
-    },
+  include ::stdlib
+  include ::apt
+
+  $pkg_host = 'https://pkg.jenkins.io'
+
+  if $::jenkins::lts  {
+    apt::source { 'jenkins':
+      location => "${pkg_host}/debian-stable",
+      release  => 'binary/',
+      repos    => '',
+      include  => {
+        'src' => false,
+      },
+      key      => {
+        'id'     => '63667EE74BBA1F0A08A698725BA31D57EF5975CA',
+        'source' => "${pkg_host}/debian/jenkins-ci.org.key",
+      },     
+      notify   => Exec['apt_update'],
+    }
+  }
+  else {
+    apt::source { 'jenkins':
+      location => "${pkg_host}/debian",
+      release  => 'binary/',
+      repos    => '',
+      include  => {
+        'src' => false,
+      },
+      key      => {
+        'id'     => '63667EE74BBA1F0A08A698725BA31D57EF5975CA',
+        'source' => "${pkg_host}/debian/jenkins-ci.org.key",
+      },
+      notify   => Exec['apt_update'],
+    }
   }
 }
