103477eeebf6ec147aaa69f8705d37cc9fc09915
lozeki <minhtri.ng@gmail.com>
2023-09-01 15:13:57 -0400

    Update cli.pp

--- manifests/cli.pp
+++ manifests/cli.pp
@@ -1,13 +1,9 @@
-# Class: jenkins::cli
-#
-# Allow Jenkins commands to be issued from the command line
-#
+# @summary Allow Jenkins commands to be issued from the command line
+# @api private
 class jenkins::cli {
-  if $caller_module_name != $module_name {
-    fail("Use of private class ${name} by ${caller_module_name}")
-  }
+  assert_private()
 
-  include ::jenkins
+  include jenkins
 
   # XXX Classes/defines which include the jenkins::cli class assume that they
   # can use the cli even if $::jenkins::cli == false.  This breaks the top
@@ -18,46 +14,50 @@ class jenkins::cli {
   #
   # As an attempt to preserve backwards compatibility, there are includes and
   # resource relationships being scattered throughout this module.
-  Class['jenkins::service'] ->
-    Class['jenkins::cli'] ->
-      Anchor['jenkins::end']
+  if $jenkins::manage_service {
+    Class['jenkins::service']
+    -> Class['jenkins::cli']
+    -> Anchor['jenkins::end']
+  }
 
   $jar = "${jenkins::libdir}/jenkins-cli.jar"
-  $grep_jarname = "JARNAME=\$(jar -tf ${jenkins::libdir}/jenkins.war | grep 'WEB-INF/lib/cli-.*\.jar')"
-  $extract_jar = "jar -xf ${jenkins::libdir}/jenkins.war \$JARNAME"
-  $move_jar = "mv \$JARNAME ${jar}"
+  $extract_jar = "jar -xf ${jenkins::libdir}/jenkins.war WEB-INF/lib/"
+  $move_jar = "mv WEB-INF/lib/cli-*.jar ${jar}"
   $remove_dir = 'rm -rf WEB-INF'
+  $cli_tries = $jenkins::cli_tries
+  $cli_try_sleep = $jenkins::cli_try_sleep
 
-  exec { 'jenkins-cli' :
-    command => "sh -c '${grep_jarname} && ${extract_jar} && ${move_jar} && ${remove_dir}'",
-    path    => ['/bin', '/usr/bin'],
-    cwd     => '/tmp',
+  # make sure we always call Exec[jenlins-cli] in case
+  # the binary does not exist
+  exec { 'check-jenkins-cli':
+    command => '/bin/true',
     creates => $jar,
-    require => Service['jenkins'],
   }
+  ~> exec { 'jenkins-cli' :
+    command     => "${extract_jar} && ${move_jar} && ${remove_dir}",
+    path        => ['/bin', '/usr/bin'],
+    cwd         => '/tmp',
+    refreshonly => true,
+  }
+  # Extract latest CLI in case package is updated / downgraded
+  Package[$jenkins::package_name] ~> Exec['jenkins-cli']
 
   file { $jar:
     ensure  => file,
+    mode    => '0644',
     require => Exec['jenkins-cli'],
   }
 
   $port = jenkins_port()
   $prefix = jenkins_prefix()
 
-  # Provide the -i flag if specified by the user.
-  if $::jenkins::cli_ssh_keyfile {
-    $auth_arg = "-i ${::jenkins::cli_ssh_keyfile}"
-  } else {
-    $auth_arg = undef
-  }
-
   # The jenkins cli command with required parameter(s)
   $cmd = join(
     delete_undef_values([
-      'java',
-      "-jar ${::jenkins::cli::jar}",
-      "-s http://localhost:${port}${prefix}",
-      $auth_arg,
+        'java',
+        "-jar ${jar}",
+        "-s http://localhost:${port}${prefix}",
+        $jenkins::_cli_auth_arg,
     ]),
     ' '
   )
@@ -66,13 +66,13 @@ class jenkins::cli {
   exec { 'safe-restart-jenkins':
     command     => "${cmd} safe-restart && /bin/sleep 10",
     path        => ['/bin', '/usr/bin'],
-    tries       => 10,
-    try_sleep   => 2,
+    tries       => $cli_tries,
+    try_sleep   => $cli_try_sleep,
     refreshonly => true,
     require     => File[$jar],
   }
 
   # jenkins::cli::reload should be included only after $::jenkins::cli::cmd is
   # defined
-  include ::jenkins::cli::reload
+  include jenkins::cli::reload
 }
